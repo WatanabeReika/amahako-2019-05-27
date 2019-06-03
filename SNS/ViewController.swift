e94aee866a8b36c1f69d21732c8d44372061dedb
ReikaWatanabe <b1017125@fun.ac.jp>
2019-06-03 17:42:35 +0900

    aaaaa

---
+++ SNS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SNS
+//
+//  Created by 渡邊峰香 on 2019/06/03.
+//  Copyright © 2019 渡邊峰香. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBAction func showActivityView(_ sender: UIBarButtonItem) {
+        let controller = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+        self.present(controller, animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
